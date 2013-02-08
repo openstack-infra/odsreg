32e0a726870f114c7f4be3987c0d06f4c3ad5908
Thierry Carrez <thierry@openstack.org>
2013-02-08 11:28:02 +0100

    Fix module path to cfp.context_processors
    Fix incorrect module path in settings.py.

--- settings.py
+++ settings.py
@@ -59,7 +59,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     "django.core.context_processors.i18n",
     "django.core.context_processors.media",
     "django.core.context_processors.request",
-    "cfp.context_processors.event",
+    "odsreg.cfp.context_processors.event",
 )
 
 MIDDLEWARE_CLASSES = (
