30b7913e2915a3ba538288e970fc75f693a3822a
Thierry Carrez <thierry@openstack.org>
2017-03-16 13:30:48 +0100

    Fix LOGIN_URL to match new style
    django.openid.auth now uses a /-terminated url route,
    so update LOGIN_URL to match.
    
    Change-Id: I19623eba5292074506f58da063b52a09b372340b

--- odsreg/settings.py
+++ odsreg/settings.py
@@ -109,7 +109,7 @@ OPENID_UPDATE_DETAILS_FROM_SREG = True
 OPENID_SSO_SERVER_URL = 'https://login.launchpad.net/'
 
 # Tell django.contrib.auth to use the OpenID signin URLs.
-LOGIN_URL = '/openid/login'
+LOGIN_URL = '/openid/login/'
 LOGIN_REDIRECT_URL = '/'
 
 # Override settings with local ones.
